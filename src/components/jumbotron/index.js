8291a8da1cd04aade3694d8593430e3c2283c5cc
Anatoly <a.boronilov@infoculture.ru>
2022-08-16 15:33:09 +0200

    jumbotron

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Inner, Item, Container, Pane, Title, SubTitle, Image } from './styles/jumbotron';
+
+const Jumbotron = ({children, direction = "row", ...restProprs}) => {
+    return (
+        <Item direction={direction}>
+            <Inner>
+                {children}
+            </Inner>            
+        </Item>
+    );
+}
+
+export default Jumbotron;
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProprs}) {
+    return <Container {...restProprs}>{children}</Container>
+}
+
+Jumbotron.Pane = function JumbotronPane({children, ...restProprs}) {
+    return <Pane {...restProprs}>{children}</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProprs}) {
+    return <Title {...restProprs}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProprs}) {
+    return <SubTitle {...restProprs}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({...restProprs}) {
+    return <Image {...restProprs}></Image>
+}

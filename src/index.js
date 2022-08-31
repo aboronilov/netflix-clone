60f5350830ad39cb996479277567153abdd57702
anatoly <boronilov@gmail.com>
2022-08-31 11:01:45 +0200

    context init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'normalize.css';
 import App from './App';
 import {GlobalStyles} from './global-styles';
 import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 // const dotenv = require('dotenv');
 // dotenv.config();
@@ -11,8 +12,10 @@ import { firebase } from './lib/firebase.prod';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>
 );
 
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,6 +1,7 @@
 import Firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+// 1) when seeding the database you'll have to uncomment this!
 // import { seedDatabase } from '../seed';
 
 const config = {
@@ -13,6 +14,8 @@ const config = {
 };
 
 const firebase = Firebase.initializeApp(config);
+// 2) when seeding the database you'll have to uncomment this!
 // seedDatabase(firebase);
+// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export {firebase};

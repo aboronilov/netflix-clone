592d56008b98505e903791295228d3f996a1cd82
anatoly <boronilov@gmail.com>
2022-09-02 12:43:06 +0200

    is user logged in routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import * as ROUTES from "./constants/routes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Home, Browse, Signin, Signup} from "./pages";
+import { IsUserRedirect } from './helpers/routes';
 
 function App() {
+  const user = null;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={ROUTES.HOME} element={<Home/>} />
-        <Route path={ROUTES.BROWSE} element={<Browse/>} />
-        <Route path={ROUTES.SIGNIN} element={<Signin/>} />
-        <Route path={ROUTES.SIGNUP} element={<Signup/>} />
+        <Route 
+          path={ROUTES.SIGNIN} 
+          element={!user ? <Signin/> : <Browse/>} 
+        />
+        <Route 
+          path={ROUTES.SIGNUP} 
+          element={!user ? <Signup/> : <Browse/>} 
+        />
+        <Route 
+          path={ROUTES.HOME} 
+          element={!user ? <Home/> : <Browse/>} 
+        />
+        <Route 
+          path={ROUTES.BROWSE} 
+          element={!user ? <Signup/> : <Browse/>} 
+        />
       </Routes>
     </BrowserRouter>
   );

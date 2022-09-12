aef16e05225ad62f397ad2b5e27b082fb2232d16
anatoly <boronilov@gmail.com>
2022-09-12 10:27:56 +0200

    films and series card

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,22 +2,29 @@ import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 import useAuthListener from '../hooks/use-auth-listener';
-import { Header, Loading } from '../components';
+import { Header, Loading, Card } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 export default function BrowseContainer({ slides }) {
+    const [category, setCategory] = useState("series");
     const [searchTerm, setSearchTerm] = useState("");
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
     const { firebase } = useContext(FirebaseContext);
+    const [slideRows, seSlideRows] = useState([]);
     const user = firebase.auth().currentUser || {};
 
     useEffect(() => {
         setTimeout(() => {
             setLoading(false)
         }, 3000)
-    }, [profile.displayName])
+    }, [profile.displayName]);
+
+    useEffect(() => {
+        seSlideRows(slides[category]);
+    }, [slides, category])
+
 
     return profile.displayName ? (
         <>
@@ -30,13 +37,23 @@ export default function BrowseContainer({ slides }) {
                 <Header.Frame>
                     <Header.Group>
                         <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-                        <Header.TextLink>Series</Header.TextLink>
-                        <Header.TextLink>Films</Header.TextLink>
+                        <Header.TextLink
+                            active={category === "series" ? "true" : "false"}
+                            onClick={() => setCategory("series")}
+                        >
+                            Series
+                        </Header.TextLink>
+                        <Header.TextLink
+                            active={category === "films" ? "true" : "false"}
+                            onClick={() => setCategory("films")}
+                        >
+                            Films
+                        </Header.TextLink>
                     </Header.Group>
                     <Header.Group>
-                        <Header.Search 
-                            searchTerm={searchTerm} 
-                            setSearchTerm={setSearchTerm} 
+                        <Header.Search
+                            searchTerm={searchTerm}
+                            setSearchTerm={setSearchTerm}
                         />
                         <Header.Profile>
                             <Header.Picture src={user.photoURL} />
@@ -64,6 +81,32 @@ export default function BrowseContainer({ slides }) {
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
             </Header>
+
+            <Card.Group>
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image 
+                                        src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                                    />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            <p>
+                                Hello
+                            </p>
+                        </Card.Feature>
+                    </Card>
+                ))}
+            </Card.Group>
         </>
     ) : (
         <SelectProfileContainer
